80dd69d76ac4ea773167931f81627e404e6caa8c
Andrei Mazalo <insalespacecat@gmail.com>
2020-10-08 00:03:41 +0300

    isUnique: option 2 description and implementation

--- src/ArraysAndStrings/isUnique.java
+++ src/ArraysAndStrings/isUnique.java
@@ -1,5 +1,8 @@
 package ArraysAndStrings;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class isUnique {
 
     //Implement an algorithm to determine if a string has all unique characters
@@ -16,11 +19,32 @@ public class isUnique {
     // TimeSum = n + n + nlogn + nlogn = 2n + 2nlogn.
     // which is O(nlogn): nlogn > n and dominates it, 2 is constant.
 
-    isUnique () {
+    //Option 2:
+    //Use hashset (which is a hashmap with pairs "Key - new Object()") + cycle - O(n) cuz set.add is O(1)
 
+
+     static boolean isUnique (String phrase) {
+        HashSet<Character> checkSet = new HashSet<Character>();
+        char[] charactersArray = phrase.toCharArray();
+        System.out.println(Arrays.toString(charactersArray));
+        for(int i = 0; i < charactersArray.length; i++){
+            System.out.println(charactersArray[i]);
+            if(charactersArray[i] == ' '){
+                System.out.println("Space met");
+                continue;
+            }
+            if(!checkSet.add(charactersArray[i])){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args){
-
+        System.out.println(isUnique("abcd efj hijk"));
+        System.out.println(isUnique("Hello, my friend! How is it going?"));
+        System.out.println(isUnique("aaaaaaaaaa"));
+        System.out.println(isUnique("Yes"));
+        System.out.println(isUnique("Its okay"));
     }
 }
