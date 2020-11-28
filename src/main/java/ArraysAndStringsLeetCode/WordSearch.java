a55dd0cf43f879d85a994cd0bffbda1934512a9d
Andrei Mazalo <insalespacecat@gmail.com>
2020-11-28 19:32:45 +0300

    Arrys&Lists(WordSearch): (WIP) optimized solution

--- src/main/java/ArraysAndStringsLeetCode/WordSearch.java
+++ src/main/java/ArraysAndStringsLeetCode/WordSearch.java
@@ -1,5 +1,18 @@
 package main.java.ArraysAndStringsLeetCode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+class C {
+    int x;
+    int y;
+    C (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class WordSearch {
 
     public boolean bruteForce(char[][] board, String word){
@@ -74,6 +87,68 @@ public class WordSearch {
             }
         }
         return false;
+    }
+
+    private HashMap<Character, ArrayList<C>> prepareAMap(char[][] board, String word){
+        boolean[] wordChecker = new boolean[128];
+        for(int i = 0; i < word.length(); i++){
+            wordChecker[(int) word.charAt(i)] = true;
+        }
+        HashMap<Character, ArrayList<C>> map = new HashMap<>();
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(wordChecker[(int) board[i][j]]){
+                    if(map.get(board[i][j]) != null){
+                        ArrayList<C> list = map.get(board[i][j]);
+                        list.add(new C(i, j));
+                        map.replace(board[i][j], list);
+                    } else {
+                        ArrayList<C> list = new ArrayList<C>();
+                        list.add(new C(i, j));
+                        map.put(board[i][j], list);
+                    }
+                }
+            }
+        }
+        return map;
+    }
+
+    private boolean checkIfExists(HashMap<Character, ArrayList<C>> map, String word){
+        boolean isFound = true;
+        for(int i = 0; i < word.length()-1; i++){
+            ArrayList<C> C1 = map.get(word.charAt(i));
+            ArrayList<C> C2 = map.get(word.charAt(i+1));
+            for(C c1: C1){
+                for(C c2: C2){
+                    if(c1.x+1 == c2.x && c1.y == c2.y){
+                        isFound = true;
+                        continue;
+                    }
+                    if(c1.x-1 == c2.x && c1.y == c2.y){
+                        isFound = true;
+                        continue;
+                    }
+                    if(c1.y-1 == c2.y && c1.x == c2.x){
+                        isFound = true;
+                        continue;
+                    }
+                    if(c1.y+1 == c2.y && c1.x == c2.x) {
+                        isFound = true;
+                    } else {
+                        isFound = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return isFound;
+    }
+
+    public boolean optimized(char[][] board, String word){
+        return checkIfExists(
+                prepareAMap(board, word),
+                word
+        );
     }
 
     public static void main(String[] args){}
--- src/test/java/ArraysAndStringsLeetCode/WordSearchTests.java
+++ src/test/java/ArraysAndStringsLeetCode/WordSearchTests.java
@@ -10,15 +10,20 @@ public class WordSearchTests {
 
     @Test
     void BruteForceAlgorithmTests(){
+        /*
         char[][] arr = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
        // assertTrue(classToTest.bruteForce(arr, "ABCCED"));
         char[][] arr2 = {{'a'}};
         assertFalse(classToTest.bruteForce(arr2, "b"));
         //assertEquals("ACB", classToTest.convertBruteForce("ABC", 2));
+         */
     }
 
     @Test
     void OptimizedAlgorithmTests() {
-
+        char[][] arr = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        assertTrue(classToTest.optimized(arr, "ABCCED"));
+        char[][] arr2 = {{'a'}};
+        assertFalse(classToTest.optimized(arr2, "b"));
     }
 }
