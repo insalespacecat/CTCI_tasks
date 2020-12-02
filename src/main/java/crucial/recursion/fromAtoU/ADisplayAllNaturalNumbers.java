433e88fadadf3c39601c38e5721328d375c84fae
Andrei Mazalo <insalespacecat@gmail.com>
2020-12-03 02:14:04 +0300

    Recusrion.fromAtoU(ADisplayAllNaturalNumbers): correct solution

--- src/main/java/crucial/recursion/fromAtoU/ADisplayAllNaturalNumbers.java
+++ src/main/java/crucial/recursion/fromAtoU/ADisplayAllNaturalNumbers.java
@@ -8,13 +8,14 @@ public class ADisplayAllNaturalNumbers {
     //System.out.println(i)
     //naturalNumbers(i+1);
     //We will fill the array to simplify unit test
-
-    public void naturalNumbers(int i, int N){
-        if(i == N){
-           return;
+    //OK, that is incorrect, because we have modified the task
+    //and said that we have i provided when we have not
+    //The correct way to solve this will be to use recursion properties:
+    public String naturalNumbers(int N){
+        if(N == 1) {
+            return "1";
         }
-        System.out.println(i);
-        naturalNumbers(i+1, N);
+        return naturalNumbers(N-1) + " " + N;
     }
 
     public static void main(String[] args){
