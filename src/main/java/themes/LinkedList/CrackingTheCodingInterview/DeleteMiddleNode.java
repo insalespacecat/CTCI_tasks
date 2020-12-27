60310422497dddf97b919d8dd5aa18291db23d84
Andrei Mazalo <insalespacecat@gmail.com>
2020-12-27 17:38:48 +0300

    LinkedList(DeleteMiddleNode): solution

---
+++ src/main/java/themes/LinkedList/CrackingTheCodingInterview/DeleteMiddleNode.java
@@ -0,0 +1,15 @@
+package main.java.themes.LinkedList.CrackingTheCodingInterview;
+
+public class DeleteMiddleNode {
+    // Delete middle node:
+    // You are given the node instance from the singly-linked list
+    // Create void function which will remove the node from the list
+
+    //To eliminate the node we usually have to overwrite the pointer
+    //of the previous node, but here we don't have access to the
+    //previous node.
+    //So in order to eliminate the node as given we simply have to
+    //assign to the given node pointer the value of the next node
+    //like deleteNode(Node N)
+    //N = N.next();
+}
