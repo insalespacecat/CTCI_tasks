ac458eead5e3ee34717c96f7c5bd3c9ca7bdceb6
Andrei Mazalo <insalespacecat@gmail.com>
2021-01-09 21:55:30 +0300

    LinkenList(DeleteMiddleNode): solution implementation

--- src/main/java/themes/LinkedList/CrackingTheCodingInterview/DeleteMiddleNode.java
+++ src/main/java/themes/LinkedList/CrackingTheCodingInterview/DeleteMiddleNode.java
@@ -1,15 +1,26 @@
 package main.java.themes.LinkedList.CrackingTheCodingInterview;
 
+
 public class DeleteMiddleNode {
     // Delete middle node:
     // You are given the node instance from the singly-linked list
     // Create void function which will remove the node from the list
+    // To eliminate the node we usually have to overwrite the pointer
+    // of the previous node, but here we don't have access to the
+    // previous node.
+    // So in order to eliminate the node as given we simply have to
+    // assign to the given node pointer the value of the next node
+    // like deleteNode(Node N)
+    // N = N.next();
 
-    //To eliminate the node we usually have to overwrite the pointer
-    //of the previous node, but here we don't have access to the
-    //previous node.
-    //So in order to eliminate the node as given we simply have to
-    //assign to the given node pointer the value of the next node
-    //like deleteNode(Node N)
-    //N = N.next();
+    //You should also discuss extreme situation that the node given
+    //is the last node, in this case we will just check that the next
+    //node exists before overwriting
+
+    void deleteNode(Node node){
+        if(node.next != null) {
+            node = node.next;
+        }
+    }
+
 }
---
+++ src/main/java/themes/LinkedList/CrackingTheCodingInterview/Node.java
@@ -0,0 +1,11 @@
+package main.java.themes.LinkedList.CrackingTheCodingInterview;
+
+public class Node{
+    int value;
+    Node next;
+
+    Node(int value, Node next){
+        this.value = value;
+        this.next = next;
+    }
+}
